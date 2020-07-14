37933458a8a25348e0ae905ab5d8c65260fe326d
Dominik Deak <deak.software@gmail.com>
2020-07-14 22:54:18 +1000

    Work on the build script.

--- BuildLinux.sh
+++ BuildLinux.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
-mkdir Build
-cd Build
+scriptPath="$( cd "$(dirname "${0}")" ; pwd -P )"
+buildPath="${scriptPath}/Build"
+synergyCorePath="${scriptPath}/Synergy-Core"
 
-cmake ../Synergy-Core
+
+mkdir "${buildPath}"; cd "${buildPath}"
+
+
+cmake -S "${synergyCorePath}" -B "${buildPath}"
+
+# pushd "${synergyCorePath}"
+
+cmake --build "${buildPath}" --parallel 8
+
+# popd
+
+
+
+
+
+
+
+# pushd "${synergyCorePath}"
+
+# 	source "build/version"
+
+# 	versionString="${SYNERGY_VERSION_MAJOR}.${SYNERGY_VERSION_MINOR}.${SYNERGY_VERSION_PATCH}"
+
+# 	echo "synergy (${versionString}) ${SYNERGY_VERSION_STAGE}; urgency=medium" > "debian/changelog"
+
+# 	debuild --set-envvar SYNERGY_ENTERPRISE=ON -us -uc
+
+# 	git clean -fd
+
+# popd
+
